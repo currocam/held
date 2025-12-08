3b33e2e76361485519e64ad2c43ca7d597f300ac
Curro Campuzano <campuzanocurro@gmail.com>
2025-12-08 15:04:57 +0100

    Plot benchmark

--- plots/00-benchmark_ld_compute.py
+++ plots/00-benchmark_ld_compute.py
@@ -1,68 +1,72 @@
+#!/usr/bin/env -S uv -n run --script
 # /// script
 # requires-python = ">=3.12"
 # dependencies = [
+#     "arviz-plots==0.7.0",
 #     "matplotlib==3.10.7",
+#     "matplotlib-label-lines",
 #     "numpy==2.3.5",
 #     "pandas==2.3.3",
 #     "plotnine==0.15.1",
+#     "scienceplots==2.2.0",
 #     "seaborn==0.13.2",
 # ]
 # ///
 
-import marimo
 
 __generated_with = "0.18.3"
-app = marimo.App(width="medium")
 
+# %%
+# Initialization code that runs before all other cells
+import matplotlib
+import matplotlib.pyplot as plt
+import scienceplots
+plt.style.use('science')
+matplotlib.rcParams.update(
+    {
+        "pgf.texsystem": "pdflatex",
+        "font.family": "serif",
+        "text.usetex": True,
+        "pgf.rcfonts": False,
+    }
+)
 
-@app.cell
-def _():
-    import marimo as mo
-    import seaborn as sns
-    import matplotlib.pyplot as plt
-    import numpy as np
-    import pandas as pd
+# Aesthetics
+textwidth = 3.31314
+aspect_ratio = 6/8
+scale = 1.0
+width = textwidth * scale
+height = width * aspect_ratio
 
-    return pd, plt, sns
+# %%
+import seaborn as sns
+import numpy as np
+import pandas as pd
 
+# %%
+df = pd.read_csv("results/tables/00-benchmark_ld_compute.csv")
+df = df[df["n_samples"] > 10]
+df
 
-@app.cell
-def _(pd):
-    df = pd.read_csv("results/tables/00-benchmark_ld_compute.csv")
-    df
-    return (df,)
+# %%
+fig, ax = plt.subplots()
+for n, gr in df.groupby("n_samples"):
+    sns.regplot(
+        x=gr["n_mutations"] ** 2,
+        y=gr["runtime_seconds"],
+        scatter=True,
+        label=f"N. diploids = {n}",
+        ax=ax,
+    )
+# Labels, title, legend
+ax.set_xlabel(r"Squared \#mutations ($n^2$)")
+ax.set_ylabel("Runtime (seconds)")
+ax.set_title("Quadratic runtime")
+plt.legend()
+plt.tight_layout()
+plt.savefig("plots/00-benchmark_ld_compute.pdf")
+plt.show()
 
-
-@app.cell
-def _(df, sns):
-    g = sns.scatterplot(data=df, x="n_mutations", y="runtime_seconds", hue="n_samples")
-    g
-    return
-
-
-@app.cell
-def _(df, plt, sns):
-    sns.set(style="whitegrid", context="talk")
-    fig, ax = plt.subplots(figsize=(8, 6))
-    for n, gr in df.groupby("n_samples"):
-        sns.regplot(
-            x=gr["n_mutations"] ** 2,
-            y=gr["runtime_seconds"],
-            scatter=True,
-            label=f"n_samples = {n}",
-            truncate=False,
-            ax=ax,
-        )
-
-    # Labels, title, legend
-    ax.set_xlabel("Mutations ^ 2")
-    ax.set_ylabel("Runtime (seconds)")
-
-    plt.tight_layout()
-    plt.show()
-
-    return
-
-
-if __name__ == "__main__":
-    app.run()
+# %%
+matplotlib.use("pgf")
+plt.savefig("plots/00-benchmark_ld_compute.pgf")
